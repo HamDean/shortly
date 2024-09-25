3b727ad575b3ceefa8a037195388290f2609252c
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-09-25 14:11:41 +0000

    Build a resusable media component

---
+++ src/components/Media.tsx
@@ -0,0 +1,23 @@
+
+interface Props {
+    cardImage : string,
+    title : string,
+    tag: string
+}
+
+const Media = ({ cardImage, title, tag } : Props) => {
+  return (
+    <div className="mt-20 w-[327px] h-[267px] bg-green-300 rounded-lg flex flex-col items-center px-[32px] py-[41px] relative">
+      <div className="bg-[#3A3054] w-[88px] h-[88px] rounded-full flex flex-col items-center justify-center absolute bottom-56">
+        <img src={cardImage} alt="brand recognition logo" className="w-[40px] h-[40px]" />
+      </div>
+
+      <h3 className="text-[22px] font-bold text-[#34313D] mt-10 mb-3">{title}</h3>
+
+      <p className="text-[#9E9AA8] text-center">
+       {tag}
+      </p>
+    </div>
+  );
+};
+export default Media;
