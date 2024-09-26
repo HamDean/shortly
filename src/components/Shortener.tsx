ed5b3775d78edcc2607e112ced82bc26732eee90
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-09-26 15:07:04 +0000

    Implement getting short url logic

--- src/components/Shortener.tsx
+++ src/components/Shortener.tsx
@@ -1,18 +1,64 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface UrlList {
+  originalUrl: string;
+  shortenedUrl: string;
+}
+
 const Shortener = () => {
+  const [url, setUrl] = useState("");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [urlList, setUrlList] = useState<UrlList[]>([]);
+
+  const getShortenedLink = async (originalURL: string) => {
+    const options = {
+      method: "POST",
+      url: "https://api.short.io/links",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        Authorization: "sk_fX7tYooZguPcsu3u",
+      },
+      data: {
+        originalURL: originalURL,
+        domain: "gfty.short.gy",
+      },
+    };
+
+    if (originalURL) {
+      try {
+        const response = await axios.request(options);
+        setUrlList((prevUrlList) => [
+          ...prevUrlList,
+          { originalUrl: originalURL, shortenedUrl: response.data.shortURL },
+        ]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+
   return (
     <section className="relative bottom-40">
       <div className="md:max-w-[1108px] mx-5 md:mx-20 lg:mx-32 flex flex-col gap-3 lg:gap-0 lg:flex-row bg-boostmob bg-[#3A3054] rounded-lg mt-20 p-5 lg:px-[64px] lg:py-[54px] lg:justify-between">
         <input
+          onChange={(e) => setUrl(e.target.value)}
           type="text"
           placeholder="Shorten a link here..."
           className="px-4 lg:px-[32px] bg-white py-1 lg:py-[14px] h-[48px] lg:h-[64px] rounded-lg outline-none text-gray-700 lg:w-full lg:mr-[24px]"
         />
 
-        <button className="bg-[#2BD0D0] text-white font-semibold text-lg hover:bg-[#9AE3E3] transition-all px-3 py-1 h-[48px] lg:w-[188px] lg:h-[64px] rounded-lg">
+        <button
+          onClick={() => getShortenedLink(url)}
+          className="bg-[#2BD0D0] text-white font-semibold text-lg hover:bg-[#9AE3E3] transition-all px-3 py-1 h-[48px] lg:w-[188px] lg:h-[64px] rounded-lg"
+        >
           Shorten it!
         </button>
       </div>
     </section>
   );
 };
+
 export default Shortener;
